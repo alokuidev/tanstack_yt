cb2cab6631a01eae8e2a459d6aa93d27c3d6b2e4
warrioralok <34855411+warrioralok@users.noreply.github.com>
2024-12-13 01:12:19 +0530

    pagination added

--- src/Component/API/Api.jsx
+++ src/Component/API/Api.jsx
@@ -2,19 +2,21 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL:'https://jsonplaceholder.typicode.com'
-})
+  baseURL: "https://jsonplaceholder.typicode.com",
+});
 
-export const fetchData = () =>{
-    return api.get('/posts')
-}
+export const fetchData = () => {
+  return api.get("/posts");
+};
 
-export const getData = async() =>{
-     const res = await api.get('/posts')
-     return res.status === 200 && Array.isArray(res.data) ? res.data : [];
-}
+export const getData = async (page) => {
+  const pagenum = page === 1 ? 0 : (page - 1) * 5;
+  const res = await api.get(`/posts?_start=${pagenum}&_limit=5`);
+  return res.status === 200 && Array.isArray(res.data) ? res.data : [];
+};
+
 // to fetch respective id post
-export const getPost = async(id) =>{
-    const res = await api.get(`/posts/${id}`)
-    return res.status === 200 ? res.data : [];
-}
+export const getPost = async (id) => {
+  const res = await api.get(`/posts/${id}`);
+  return res.status === 200 ? res.data : [];
+};
--- src/Component/Pages/FetchRQ.jsx
+++ src/Component/Pages/FetchRQ.jsx
@@ -1,38 +1,59 @@
-
 import { NavLink } from "react-router-dom";
 import { getData } from "../API/Api";
 import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
 
-export const FetchRQ = () =>{
-
-    
-
-    const {data, isLoading , isError , error} = useQuery({
-        queryKey:['post'],
-        queryFn:getData,
-        //staleTime:500000,
-        refetchInterval:1000,
-        refetchIntervalInBackground:true,
-    })
-    if(isLoading) return <p className="status">Loading...</p>
-    if(isError) return <p className="status"> Error: {error.message || 'Something Went Wrong !!!'}</p>
-    return(
-        <>
-            <ul>
-             {  data?.map((currElem) =>{  
-                const {id, title, body} = currElem; 
-              return(  
-                <li key={id}>
-                    <NavLink to={`/rq/${id}`}>
-                        <p>{title}</p>
-                        <p>{body}</p>
-                    </NavLink>
-                </li>
-            )
-            })
-            }
-            </ul>
-        
-        </>
-    )
-}
+export const FetchRQ = () => {
+  const [pagenum, setPageNum] = useState(1);
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ["post", pagenum],
+    queryFn: () => getData(pagenum),
+    //staleTime:500000,
+    // refetchInterval:1000,
+    // refetchIntervalInBackground:true,
+  });
+  if (isLoading) return <p className="status">Loading...</p>;
+  if (isError)
+    return (
+      <p className="status">
+        {" "}
+        Error: {error.message || "Something Went Wrong !!!"}
+      </p>
+    );
+  return (
+    <>
+      <ul>
+        {data?.map((currElem) => {
+          const { id, title, body } = currElem;
+          return (
+            <li key={id}>
+              <NavLink to={`/rq/${id}`}>
+                <p>{id}</p>
+                <p>{title}</p>
+                <p>{body}</p>
+              </NavLink>
+            </li>
+          );
+        })}
+      </ul>
+      <div className="buttonContainer">
+        <button
+          onClick={() => {
+            setPageNum(pagenum - 1);
+          }}
+          disabled={pagenum === 1}
+        >
+          Prev
+        </button>
+        <p>{pagenum}</p>
+        <button
+          onClick={() => {
+            setPageNum(pagenum + 1);
+          }}
+        >
+          Next
+        </button>
+      </div>
+    </>
+  );
+};
